763860a35e6e5ea7ded6970517216f1a0b67c708
Elliot Smith <elliot.smith@my.jcu.edu.au>
2016-10-30 14:28:04 +1000

    GUI Framework

---
+++ src/GUIControl.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+/**
+ * Created by aqueries on 30/10/2016.
+ */
+public class GUIControl
+{
+    static Game instance;
+    static final int width = 800;
+    static final int height = 300;
+
+
+    public static void getGameInstance(Game gameInstance)
+    {
+        instance = gameInstance;
+    }
+//    public static void openSetp
+
+}
